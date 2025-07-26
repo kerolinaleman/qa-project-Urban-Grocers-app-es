902acc56a3daceb420a49b238af6ea4f49283834
Kerolin Alemán Rivera <kerolinaleman@gmail.com>
2025-07-26 17:42:19 -0600

    Update configuration.py

--- configuration.py
+++ configuration.py
@@ -1,3 +1,3 @@
-URL_SERVICE = # Inserta tu dirección de URL sin la barra diagonal al final
-CREATE_USER_PATH = # Almacena la ruta para crear un usuario o usuaria en esta variable
-KITS_PATH = # Almacena la ruta para crear un kit en esta variable
+URL_SERVICE = "https://cnt-efc7eb1b-dd63-444a-9075-8f1ec4d5083d.containerhub.tripleten-services.com/"
+CREATE_USER_PATH = "/api/v1/users"
+KITS_PATH = "/api/v1/kits"
