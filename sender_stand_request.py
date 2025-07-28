609b64c6899c485151b3af650ae5f9b7715ccfb0
Kerolin Alemán Rivera <kerolinaleman@gmail.com>
2025-07-28 00:26:34 -0600

    Update sender_stand_request.py

--- sender_stand_request.py
+++ sender_stand_request.py
@@ -2,17 +2,22 @@ import requests
 import configuration
 import data
 
-def post_new_user (body):
+
+def post_new_user ():
 	return requests.post(configuration.URL_SERVICE + configuration.CREATE_USER_PATH,
-	                     json=body,
-			             headers=data.headers)
+	                     json=data.user_body,
+			             headers=data.header_user)
 
 def post_new_client_kit(kit_body, auth_token):
-	new_headers = data.headers.copy()
+	new_headers = data.header_user.copy()
 	new_headers["Authorization"] = "Bearer " + auth_token
 	return requests.post(configuration.URL_SERVICE + configuration.CREATE_USER_PATH,
 	                     json=kit_body,
 			             headers=new_headers)
 
+def post_new_kit (current_header, body):
+	return requests.post(configuration.URL_SERVICE + configuration.KITS_PATH,
+						 json= body,
+						 headers= current_header)
 
-
+# Def. el authToken
