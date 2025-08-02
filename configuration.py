69e2bd0ecb080622a6384938edf90e9aa3c812dd
Kerolin Alemán Rivera <kerolinaleman@gmail.com>
2025-08-01 20:15:57 -0600

    Update configuration.py

--- configuration.py
+++ configuration.py
@@ -1,3 +1,11 @@
-URL_SERVICE = "https://cnt-1a2fc2fa-2db9-48ad-a2d4-fae9a6338f92.containerhub.tripleten-services.com"
+# Dirección base del servicio API (no debe tener '/' al final)
+URL_SERVICE = "https://cnt-0218e509-479b-47b4-ab13-fe8a5a628df1.containerhub.tripleten-services.com"
+
+# Ruta del endpoint para crear usuarios/as
 CREATE_USER_PATH = "/api/v1/users"
+
+# Ruta del endpoint para crear kits de productos
 KITS_PATH = "/api/v1/kits"
+
+# Ejemplo de uso en otro archivo:
+# full_url = URL_SERVICE + CREATE_USER_PATwatchH
